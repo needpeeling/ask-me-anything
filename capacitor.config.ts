15b099f80dbb8fd15e3510e2247b92c4ccef4454
needpeeling <martin.gius95@gmail.com>
2024-11-29 17:39:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ask-me-anything',
+  webDir: 'www'
+};
+
+export default config;
